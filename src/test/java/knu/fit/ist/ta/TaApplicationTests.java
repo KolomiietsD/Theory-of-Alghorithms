dffff8ca1b0063b5b8b89cd3888fa41e43cb239c
Danya <Danya@DESKTOP-VGS5CF0>
2021-05-20 23:13:44 +0300

    lab3+tests

--- src/test/java/knu/fit/ist/ta/TaApplicationTests.java
+++ src/test/java/knu/fit/ist/ta/TaApplicationTests.java
@@ -1,7 +1,10 @@
 package knu.fit.ist.ta;
-
+import knu.fit.ist.ta.lab2.Calculate;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class TaApplicationTests {
@@ -9,5 +12,17 @@ class TaApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+        @Test
+    void test1() {
+        assertEquals(0.02f, 0.025f, 0.01f);
+    }
+     @Test
+    void test2() {
+       
+       assertEquals(Float.MAX_VALUE, Calculate.lab2equation(0));           
+        assertEquals(0, Calculate.lab2equation(0.5f));              
+        assertEquals(0.05f, Calculate.lab2equation(2));             
+    
+    }
 
 }
